431fbaa2a880f966a0dfde4378d31b36ae79d1bb
Ashish Pradhan <ashish.pradhan@vit.edu.in>
2021-05-31 20:03:15 +0530

    commit on main

---
+++ nodejs/variables.js
@@ -0,0 +1,28 @@
+let a=1; //Best way
+const b=2;
+var c=3;  //This not the best way
+d=3; //never follow -because it is a global variable
+
+let arr = [1,2,3,4,5,6];
+let len = arr.length;
+console.log("The Lenght id ", len);
+console.log("The value is ", arr[len])
+
+//arrays
+//let arr = [1,2,3,4,5,"Nixon"]
+//arr[1]
+
+
+//Objects is a key pair Key:Value
+let as = {
+    ashish:21,
+    ankita:16,
+    sujit:17,
+    jesika:13
+}
+
+console.log(a,b,c,d);
+
+arr.forEach(x =>{
+    console.log(x)
+})
