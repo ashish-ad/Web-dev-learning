8b79991a1470581a377385122a687c29c3806f5e
Ashish Pradhan <ashish.pradhan@vit.edu.in>
2021-06-03 03:41:41 +0530

    commit on main

---
+++ nodejs/function.js
@@ -0,0 +1,30 @@
+// function printHello(){
+//     console.log("Hello");
+// }
+
+// printHello(); //function call
+
+// function addNumber(a,b){
+//     let sum = a+b;
+//     console.log(sum);
+//     return sum;
+// }
+
+// ashish = addNumber(3,2);
+// console.log(ashish);
+
+
+// //new way of writing the function in ES6
+
+// let doubleTheNumber = (a) => 2*a;
+
+
+// let d=doubleTheNumber(10);
+// console.log(d);
+// console.log(b);
+
+let varVariable = 10;
+let Varvaribale = 20;
+function printVarVariable(){
+    var varV
+}
