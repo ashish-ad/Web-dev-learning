e5881fa1d844051a4b86562ed337e9a11e7bfbc1
Ashish Pradhan <ashish.pradhan@vit.edu.in>
2021-06-04 03:13:47 +0530

    commit on main

--- nodejs/function.js
+++ nodejs/function.js
@@ -23,8 +23,22 @@
 // console.log(d);
 // console.log(b);
 
-let varVariable = 10;
-let Varvaribale = 20;
-function printVarVariable(){
-    var varV
+// let varVariable = 10;
+// let Varvaribale = 20;
+// function printVarVariable(){
+//     var varV
+// }
+
+// for (var i=0; i<5; i++){
+//     setTimeout(function(){
+//         console.log(i);
+//     },100);
+// }
+let i = 3;
+let j = 4;
+let k = 1;
+for (i=0; i<3; i++){
+    k += j;
+    j = j-1;
+    con
 }
