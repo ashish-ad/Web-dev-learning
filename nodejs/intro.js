57577728731795bf6ae443ce78f6064f53b7e959
Ashish Pradhan <ashish.pradhan@vit.edu.in>
2021-06-22 17:56:04 +0530

    commit on main

---
+++ nodejs/intro.js
@@ -0,0 +1,7 @@
+//GLOBALS     - NO WINDOW !!!!
+//__dirname   - path to current 
+//require     - function to use modules (common JS)
+//modules     - info about current modules (files)
+//process     - info about env where the program is being
+
+console.log(__filename)
